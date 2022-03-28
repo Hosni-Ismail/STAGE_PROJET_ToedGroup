0d359144d95606fb6b2888ef9d44a21ecadc0396
Hosni ISMAIL <hosni.ismail27@gmail.com>
2022-03-28 22:03:56 +0200

    git checkout -b TOED-13-partie-vitrine-site-web

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center">
                       <li class="breadcrumb-item"><a href="#">Accueil</a></li>
-                      <li class="breadcrumb-item active" aria-current="page">404</li>
+                      <li class="breadcrumb-item active" aria-current="page">404</li> 
                     </ol>
                 </nav>
               </div>
