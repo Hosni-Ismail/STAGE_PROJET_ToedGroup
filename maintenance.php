34015a9b0e5d96f56324e4cc8ea3188c904873d0
hosni-ismail <hosni.ismail27@gmail.com>
2022-03-29 11:53:32 +0200

    git checkout -b TOED-39-page-405-site-en-maintenance

---
+++ maintenance.php
@@ -0,0 +1,46 @@
+<?php include('include/header.php'); ?>
+
+<div id="banner-area" class="banner-area" style="background-image:url(images/banner/banner1.jpg)">
+  <div class="banner-text">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+              <div class="banner-heading">
+                <h1 class="banner-title"></h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb justify-content-center">
+                      <li class="breadcrumb-item"><a href="#">Accueil</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">405</li> 
+                    </ol>
+                </nav>
+              </div>
+          </div><!-- Col end -->
+        </div><!-- Row end -->
+    </div><!-- Container end -->
+  </div><!-- Banner text end -->
+</div><!-- Banner area end --> 
+
+<section id="main-container" class="main-container">
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-12">
+        <div class="error-page text-center">
+          <div class="error-code">
+            <h2><strong>405</strong></h2>
+          </div>
+          <div class="error-message">
+            <h3>Le site est en maintenance</h3>
+          </div>
+          <div class="error-body">
+          Nous sommes désolées pour la gene occasionnée. ToedGroup revient très vite!<br>
+          </div>
+        </div>
+      </div>
+
+    </div><!-- Content row -->
+  </div><!-- Conatiner end -->
+</section><!-- Main container end -->
+
+<?php include('include/footer.php'); ?>
